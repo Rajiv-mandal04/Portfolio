9b344c2b0e601e7d850949ff2553cd9904a00899
Rajiv kr. Mandal <mandalrajiv2005@gmail.com>
2025-05-26 12:01:45 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("siya").addEventListener("click", function() {
+        document.querySelector(".mainly").scrollIntoView({ behavior: "smooth" });
+    });
+});
